572c10be2cc6951676148f3d2cbf76e3fc397ed7
Jose Ulloa <contacto@jud.cl>
2022-11-29 22:10:04 -0300

    Api consume + single entry fail test part 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,14 +3,14 @@ import NavBar from './NavBar/NavBar'
 import Main from './Main/Main'
 import SlideshowHero from './SlideshowHero/SlideshowHero'
 import ItemListContainer from './ItemListContainer/ItemListContainer'
-import ItemDetailContainer from './ItemDetailContainer/ItemDetailContainer'
+/* import ItemDetailContainer from './ItemDetailContainer/ItemDetailContainer' */
 
 const App = () => {
 	return (
 		<>
 			<NavBar/>
 			<Main>
-				<ItemDetailContainer/>
+				{/* <ItemDetailContainer/> */}
 				<SlideshowHero/>
 				<ItemListContainer greeting={"Jud Store"}/>
 			</Main>
